d8fcf7a65cb4939ff16a2c61fadcb71362650ffc
Sebastian Bock <sebastianbock@sbmpr.local>
2014-10-02 15:46:31 +0200

    Initial Commit

---
+++ crypterOSX/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  crypterOSX
+//
+//  Created by Sebastian Bock on 02.10.14.
+//  Copyright (c) 2014 Apptivity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
